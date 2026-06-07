b6cfd7b96d9afa09a4f77f2c9bf52007376140e2
Lobo <lobatox4@gmail.com>
2026-06-07 16:04:00 -0300

    Proyecto inicial

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>📚 Mi Manga Tracker</h1>
+
+      <p>
+        Próximamente:
+      </p>
+
+      <ul>
+        <li>Buscar mangas</li>
+        <li>Agregar a colección</li>
+        <li>Marcar tomos comprados</li>
+      </ul>
+    </main>
+  );
+}
